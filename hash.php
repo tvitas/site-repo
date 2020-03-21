7382d42e9acf45172a0f23768f3d7be534b33a03
tvitas <vitalijus.trainys@gmail.com>
2020-03-22 01:00:19 +0200

    Password hash

--- hash.php
+++ hash.php
@@ -1,2 +1,8 @@
 <?php
-echo password_hash($argv[1], PASSWORD_DEFAULT) . PHP_EOL;
+if (isset($argv[1]) and '' !== $argv[1]) {
+    echo password_hash($argv[1], PASSWORD_DEFAULT) . PHP_EOL;
+    exit(0);
+}
+echo 'Usage: ' . $argv[0] . ' [password]';
+exit(1);
+
